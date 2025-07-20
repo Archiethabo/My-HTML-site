ec9d850e3f1112aa6d0f3cf6e8832dfb3c5216fb
Archiethabo <leanothbz@gmail.com>
2025-07-20 15:53:40 +0200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+
+// Paste your actual Firebase config here
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "Moblue",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
